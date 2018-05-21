d6477b57993a84537ac7b40a9f25bf7c1dae279c
Jeremy Danyow <jdanyow@gmail.com>
2018-05-20 23:57:24 -0700

    sync with slack url format, use mutation observer

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,15 @@
 function removeRedirects() {
-  var links = document.querySelectorAll('a[onclick^=this\\.href\\=\\"https\\:\\/\\/slack-redir]'),
-      link,
-      i = links.length;
-  while (i--) {
-    link = links[i];
-    link.removeAttribute('onclick');
-    link.parentNode.replaceChild(link.cloneNode(true), link);
-  }
+  Array.from(document.querySelectorAll('a[href^="https://slack-redir.net/link?url="'))
+    .forEach(a => a.href = decodeURIComponent(a.href.substr(5)));
 }
 
-setInterval(removeRedirects, 300);
+let timeout = 0;
+function schedule() {
+  clearTimeout(timeout);
+  timeout = setTimeout(removeRedirects, 300);
+}
+
+new MutationObserver(removeRedirects)
+  .observe(document.body, { childList: true });
+
+removeRedirects();
